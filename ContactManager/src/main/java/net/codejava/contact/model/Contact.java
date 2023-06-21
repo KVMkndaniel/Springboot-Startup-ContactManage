6075817ea5f78e827c11c16b4567085023e9cf31
Manikandan M <115469323+KVMkndaniel@users.noreply.github.com>
2023-06-22 01:07:40 +0530

    Add files via upload

---
+++ ContactManager/src/main/java/net/codejava/contact/model/Contact.java
@@ -0,0 +1,74 @@
+package net.codejava.contact.model;
+
+public class Contact {
+
+	private Integer contact_id;
+	private String name;
+	private String email;
+	private String address;
+	private String phone;
+
+	public Contact(Integer contact_id, String name, String email, String address, String phone) {
+		
+		this(name,email,address,phone);
+		this.contact_id = contact_id;
+
+	}
+	public Contact( String name, String email, String address, String phone) {
+		
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.phone = phone;
+	}
+
+
+	public Integer getContact_id() {
+		return contact_id;
+	}
+
+	public void setContact_id(Integer contact_id) {
+		this.contact_id = contact_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	@Override
+	public String toString() {
+		return "Contact [contact_id=" + contact_id + ", name=" + name + ", email=" + email + ", address=" + address
+				+ ", phone=" + phone + "]";
+	}
+
+	
+	
+	
+}
